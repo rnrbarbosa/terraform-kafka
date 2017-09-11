6ea671f349c1300c461bae0ee5958bf966944bc6
Roberto Barbosa <rnrbarbosa@gmail.com>
2017-09-11 22:20:35 +0200

    corrected vpc enable(d) dns support

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "main" {
   cidr_block = "172.16.0.0/16"
 
-  enabled_dns_support   = true
-  enabled_dns_hostnames = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags {
     Name = "My VPC"  
   }
 }
 
-resource "aws_internet_gateway" {    
-    vpc_id = ${aws_vpc.main.id}
+resource "aws_internet_gateway" "igw" {    
+    vpc_id = "${aws_vpc.main.id}"
   tags {
     Name = "IGW"
   }
