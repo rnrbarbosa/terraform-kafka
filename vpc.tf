7a45a4e72ba45867470d813f63f4467846867550
Roberto Barbosa <rnrbarbosa@gmail.com>
2017-09-11 18:36:28 +0200

    added a AWS Internet Gateway

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" {
+resource "aws_vpc" "main" {
   cidr_block = "172.16.0.0/16"
 
   enabled_dns_support   = true
@@ -6,5 +6,12 @@ resource "aws_vpc" {
 
   tags {
     Name = "My VPC"  
+  }
+}
+
+resource "aws_internet_gateway" {    
+    vpc_id = ${aws_vpc.main.id}
+  tags {
+    Name = "IGW"
   }
 }
